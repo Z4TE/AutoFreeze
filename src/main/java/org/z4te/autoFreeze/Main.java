e9be4d514f1afa74a1ba1f60385890096f9165af
Z4TE <kayakun426@gmail.com>
2024-12-16 12:10:11 +0900

    日照サイクル停止/再開の処理を実装

--- src/main/java/org/z4te/autoFreeze/Main.java
+++ src/main/java/org/z4te/autoFreeze/Main.java
@@ -1,8 +1,16 @@
 package org.z4te.autoFreeze;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.GameRule;
+import org.bukkit.World;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class Main extends JavaPlugin {
+public final class Main extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
@@ -13,5 +21,26 @@ public final class Main extends JavaPlugin {
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        if (Bukkit.getOnlinePlayers().size() == 1) {
+            World world = getServer().getWorld("world");
+            assert world != null;
+            world.setGameRule(GameRule.DO_DAYLIGHT_CYCLE, true);
+            Bukkit.broadcastMessage(ChatColor.YELLOW + "Daylight cycle resumed");
+            Bukkit.getLogger().info("Daylight cycle resumed");
+        }
+    }
+
+    @EventHandler
+    public void onPlayerLeave(PlayerQuitEvent event) {
+        if (Bukkit.getOnlinePlayers().isEmpty()) {
+            World world = getServer().getWorld("world");
+            assert world != null;
+            world.setGameRule(GameRule.DO_DAYLIGHT_CYCLE, false);
+            Bukkit.getLogger().info("Daylight cycle paused");
+        }
     }
 }
